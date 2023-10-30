e896c1c470b6e118cf73ca6dff7f9ab07668bc49
Gustavo Do Espirito Santo <gustaeps@gmail.com>
2023-10-30 19:46:15 -0300

    Feat: Config de email

--- orion-bank-api/src/Middleware/ConfigurarEmail.ts
+++ orion-bank-api/src/Middleware/ConfigurarEmail.ts
@@ -19,15 +19,27 @@ function ConfigurarEmail() : nodemailer.Transporter<SMTPTransport.SentMessageInf
     })
 }
 
-export async function EnviarEmail(emailConta: string, nome: string) : Promise<void> {
+export async function EnviarEmail(emailConta: string, nome: string, senha: string) : Promise<void> {
     const smtp = ConfigurarEmail()
     
     const email = process.env.EMAIL;
+    const contaAprovada = `<h3>
+                    <strong>PARABÉNS ${nome}</strong>,
+                    sua conta foi aprovada!
+                </h3>
+                <p>
+                    Para dar continuidade com sua conta, peço que acesse o link abaixo para alterar sua senha de acesso!
+                </p>
+                <p>
+                    Senha para primeiro acesso: ${senha}
+                </p>
+                <a href="https://www.google.com.br">Clique aqui!</a>`;
+
     const configEmail = {
         from: email,
         to: emailConta,
         subject: "OrionBank - CONTA APROVADA",
-        html: `<p>prezado/a ${nome}, sua conta foi aprovada!</p>`
+        html: contaAprovada
     }
 
    await smtp.sendMail(configEmail)
