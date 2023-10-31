a288a73c509085d258cc242ed928c86e19af3ba2
Gustavo Do Espirito Santo <gustaeps@gmail.com>
2023-10-30 21:03:37 -0300

    Feat: Enviar email com imagem

--- orion-bank-api/src/Middleware/ConfigurarEmail.ts
+++ orion-bank-api/src/Middleware/ConfigurarEmail.ts
@@ -23,23 +23,40 @@ export async function EnviarEmail(emailConta: string, nome: string, senha: strin
     const smtp = ConfigurarEmail()
     
     const email = process.env.EMAIL;
-    const contaAprovada = `<h3>
-                    <strong>PARABÉNS ${nome}</strong>,
-                    sua conta foi aprovada!
-                </h3>
-                <p>
-                    Para dar continuidade com sua conta, peço que acesse o link abaixo para alterar sua senha de acesso!
-                </p>
-                <p>
-                    Senha para primeiro acesso: ${senha}
-                </p>
-                <a href="https://www.google.com.br">Clique aqui!</a>`;
+    const contaAprovada =  `<h3>
+                                <strong>PARABÉNS ${nome}</strong>,
+                                sua conta foi aprovada!
+                            </h3>
+                            <p>
+                                Para dar continuidade com sua conta, peço que acesse o link abaixo para alterar sua senha de acesso!
+                            </p>
+                            <p>
+                                Senha para primeiro acesso: ${senha}
+                            </p>
+                            <a href="https://www.google.com.br">Clique aqui!</a>  
+                            <br/>
+                            <br/>
+                            <img 
+                                src="cid:${email}"
+                                alt="Imagem logo orion bank"
+                                style="
+                                    width: 350px; 
+                                    height: 100px
+                                ;" 
+                            />`;
 
     const configEmail = {
         from: email,
         to: emailConta,
-        subject: "OrionBank - CONTA APROVADA",
-        html: contaAprovada
+        subject: "Orion Bank - CONTA APROVADA",
+        html: contaAprovada,
+        attachments: [
+            {
+                filename: "logo-orion-bank.png",
+                path: "src/Middleware/Logo/logo-orion-bank.png", 
+                cid: email, 
+            }
+        ]
     }
 
    await smtp.sendMail(configEmail)
